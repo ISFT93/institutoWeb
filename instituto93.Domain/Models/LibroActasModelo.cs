4818b697ea59c003890f5db7f1468f8c7d811493
V4len005 <ibarravalentino15@gmail.com>
2026-06-10 17:03:32 -0300

    Clase LibroActasModelo

---
+++ instituto93.Domain/Models/LibroActasModelo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace instituto93.Domain.Models
+{
+    public class LibroActasModelo
+    {
+        //[Clave]
+        public int LibroActaId { get; set; }
+        public int LibroNumero { get; set; }
+        public int FolioNumero { get; set; }
+        public int FolioMaximo { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public DateTime? FechaBaja { get; set; }
+        public bool Activo { get; set; }
+        public int TipoLibroId { get; set; }
+    }
+}
