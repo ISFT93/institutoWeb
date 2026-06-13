fd66c7d7e79f6202b6dbabb201390285600de9fe
Franco Daniel Fernandez <133538118+FrancoDF7@users.noreply.github.com>
2026-06-13 18:56:53 -0300

    Agregado de nombre de alumno en clase

--- instituto93.Domain/Models/InscripcionMateriasModelo.cs
+++ instituto93.Domain/Models/InscripcionMateriasModelo.cs
@@ -8,6 +8,7 @@ namespace instituto93.Domain.Models
 {
     public class InscripcionMateriasModelo
     {
+        //Fernandez Franco
         public int cursadaAlumnoId { get; set; }
         public string estado { get; set; }
         public string cursada { get; set; }
