7ba42d9801a0dfb86ff118f63e9f282716e413d1
4l3j0Ok <alejofsarmiento@gmail.com>
2026-06-19 17:33:36 -0300

    feat: parametro implementation based on localidad

---
+++ instituto93.Domain/Models/Parametros.cs
@@ -0,0 +1,14 @@
+using instituto93.Domain.Interfaces;
+
+namespace instituto93.Domain.Models
+{
+    public class Parametros:IParametros
+    {
+        public int ParametroId { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Valor { get; set; }
+        public Int16 TipoId { get; set; }
+        public bool Activo { get; set; }
+    }
+}
