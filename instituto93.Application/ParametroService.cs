9160f9c3222e90d566d7557b9968ec2edd22ecd7
torvalds <torvalds@linux-foundation.org>
2026-06-26 21:47:36 -0300

    fix: rename parametro files to singular

--- instituto93.Application/ParametroService.cs
+++ instituto93.Application/ParametroService.cs
@@ -4,34 +4,34 @@ using instituto93.Domain.Models;
 
 namespace instituto93.Application
 {
-    public class ParametrosService : IParametrosService
+    public class ParametroService : IParametroService
     {
-        private readonly IParametrosRepository _repo;
+        private readonly IParametroRepository _repo;
 
-        public ParametrosService(IParametrosRepository repo)
+        public ParametroService(IParametroRepository repo)
         {
             _repo = repo ?? throw new ArgumentNullException(nameof(repo));
         }
 
-        public async Task<List<Parametros>> GetParametrosAsync(CancellationToken cancellationToken = default)
+        public async Task<List<Parametro>> GetParametrosAsync(CancellationToken cancellationToken = default)
         {
             var result = await _repo.GetAllAsync(cancellationToken);
             return result.ToList();
         }
 
-        public Task<Parametros?> GetParametroByIdAsync(int id, CancellationToken cancellationToken = default)
+        public Task<Parametro?> GetParametroByIdAsync(int id, CancellationToken cancellationToken = default)
             => _repo.GetByIdAsync(id, cancellationToken);
 
-        public Task<Parametros?> GetParametroByNameAsync(string nombre, CancellationToken cancellationToken = default)
+        public Task<Parametro?> GetParametroByNameAsync(string nombre, CancellationToken cancellationToken = default)
             => _repo.GetByNameAsync(nombre, cancellationToken);
 
-        public Task<int> CreateParametroAsync(Parametros parametros, CancellationToken cancellationToken = default)
+        public Task<int> CreateParametroAsync(Parametro parametros, CancellationToken cancellationToken = default)
             => _repo.CreateAsync(parametros, cancellationToken);
 
-        public Task<bool> UpdateParametroAsync(Parametros parametros, CancellationToken cancellationToken = default)
+        public Task<bool> UpdateParametroAsync(Parametro parametros, CancellationToken cancellationToken = default)
             => _repo.UpdateAsync(parametros, cancellationToken);
 
-        public Task<bool> UpdateParametrosRangeAsync(IEnumerable<Parametros> parametros, CancellationToken cancellationToken = default)
+        public Task<bool> UpdateParametrosRangeAsync(IEnumerable<Parametro> parametros, CancellationToken cancellationToken = default)
             => _repo.UpdateRangeAsync(parametros, cancellationToken);
 
         public Task<bool> DeleteParametroAsync(int id, CancellationToken cancellationToken = default)
