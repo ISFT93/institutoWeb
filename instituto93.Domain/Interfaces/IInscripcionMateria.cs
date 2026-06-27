e0ccf048fd5830981bb16dee5eeb0c7f221794ee
Franco Daniel Fernandez <133538118+FrancoDF7@users.noreply.github.com>
2026-06-27 20:33:25 -0300

    Agrega clase e interface de InscripcionMateria

---
+++ instituto93.Domain/Interfaces/IInscripcionMateria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace instituto93.Domain.Interfaces
+{
+    public interface IInscripcionMateria
+    {
+        int cursadaAlumnoId { get; set; }
+        string estado { get; set; }
+        string cursada { get; set; }
+    }
+}
