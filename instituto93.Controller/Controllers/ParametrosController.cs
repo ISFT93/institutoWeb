d610ef74242e07a16583d626388c94b770d3c25e
4l3j0Ok <alejofsarmiento@gmail.com>
2026-06-26 21:20:50 -0300

    fix: add fault ParametrosController

---
+++ instituto93.Controller/Controllers/ParametrosController.cs
@@ -0,0 +1,36 @@
+﻿using instituto93.Application;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using instituto93.Domain.Models;
+using instituto93.Application.Interfaces;
+
+namespace instituto93.Controller.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ParametrosController : ControllerBase
+    {
+        private readonly IParametrosService _service;
+
+        public ParametrosController(IParametrosService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Parametros>>> Get(CancellationToken cancellationToken)
+        {
+            try
+            {
+                var parametros = await _service.GetParametrosAsync(cancellationToken);
+                return Ok(parametros);
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
