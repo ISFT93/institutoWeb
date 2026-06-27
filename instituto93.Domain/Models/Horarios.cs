d65031b57e91dd6fa25e74f283a056344ccdceea
ignacio <nacho42140183@gmail.com>
2026-06-26 21:37:21 -0300

    Horario Clase Corregida

---
+++ instituto93.Domain/Models/Horarios.cs
@@ -0,0 +1,22 @@
+﻿using instituto93.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace instituto93.Domain.Models
+{
+    //Mourdad Ignacio 
+    public class Horarios:IHorario
+    {
+        public int HorarioId { get; set; }
+        public int? DiaId { get; set; }
+        public int? ModuloId { get; set; }
+        public int CursoMateriaId { get; set; }
+       
+        public int MateriaId { get; set; }
+        public int CursoId { get; set; }
+        public string Nombre { get; set; }
+    }
+}
