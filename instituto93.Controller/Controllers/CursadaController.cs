277ac09bff18d771ec07d676e169c8864e1041f3
Santiago2322 <tu-email@ejemplo.com>
2026-07-03 14:38:34 -0300

    cursada

---
+++ instituto93.Controller/Controllers/CursadaController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using instituto93.Domain.Models;
+using instituto93.Application.Interfaces;
+using System.Threading.Tasks;
+
+namespace instituto93.Controller.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CursadaController : ControllerBase
+    {
+        private readonly ICursadaService _service;
+        public CursadaController(ICursadaService service) => _service = service;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() => Ok(await _service.GetAllAsync());
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var res = await _service.GetByIdAsync(id);
+            return res == null ? NotFound() : Ok(res);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Cursada model)
+        {
+            await _service.AddAsync(model);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] Cursada model)
+        {
+            await _service.UpdateAsync(model);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _service.DeleteAsync(id);
+            return Ok();
+        }
+    }
+}
---
+++ instituto93.Domain/Models/Cursada.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace instituto93.Domain.Models
+{
+    public class Cursada
+    {
+        
+        public int CursadaId { get; set; }
+        public int CursoMateriaId { get; set; }
+        public required string AnioLectivo { get; set; }
+        public required string Anio { get; set; }
+        public int CantidadAlumnos { get; set; }
+        public int CantidadAlumnosRecursantes { get; set; }
+        public int CantidadAlumnosDesertores { get; set; }
+        public int HoraCatedra { get; set; }
+        public DateTime FechaAsistencia { get; set; }
+        public int PorcentajeAsistencia { get; set; }
+        public int CursoId { get; set; }
+        public required string Materia { get; set; }
+        public int ModulosMateria { get; set; }
+    }
+}
