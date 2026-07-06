761abe52c0f4a512a1e12996cb2f68bc0d106514
Franco Daniel Fernandez <133538118+FrancoDF7@users.noreply.github.com>
2026-07-06 15:39:51 -0300

    Soporte DI para InscripcionMateriaController

--- instituto93.Controller/Program.cs
+++ instituto93.Controller/Program.cs
@@ -1,4 +1,5 @@
 using instituto93.Application;
+using instituto93.Application.Interfaces;
 using instituto93.Data;
 using instituto93.Data.Repositories;
 using Microsoft.OpenApi.Models;
@@ -8,7 +9,7 @@ using WebApplication1.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configuración simple: puedes mover esto a appsettings.json / User Secrets
+// Configuraciï¿½n simple: puedes mover esto a appsettings.json / User Secrets
 builder.Configuration["Jwt:Secret"] ??= "4d6d6a6d6a6d6a6d6a6d6a6d6a6d6a6d6a6d6a6d6a6d6a6d6a6d6a6d6a6d6a6d";
 
 // Add services to the container.
@@ -16,6 +17,8 @@ builder.Services.AddControllers();
 builder.Services.AddTransient<instituto93.Data.Conexion>();
 builder.Services.AddScoped<instituto93.Data.Repositories.ILocalidadRepository, instituto93.Data.Repositories.LocalidadRepository>();
 builder.Services.AddScoped<ILocalidadService, LocalidadService>();
+builder.Services.AddScoped<instituto93.Data.Repositories.Interfaces.IInscripcionMateriaRepository, instituto93.Data.Repositories.InscripcionMateriaRepository>();
+builder.Services.AddScoped<IInscripcionMateriaService, InscripcionMateriaService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
