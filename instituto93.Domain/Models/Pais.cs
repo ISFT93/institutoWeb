edab59fe0fa268c6188dbc0ab7d0117c4d1306ed
nicos92 <nicolassandoval140692@gmail.com>
2026-07-06 06:46:33 -0300

    Agrega interfaz Ipais
    - Limpia el modelo de entidad Pias e implementa IPais

--- instituto93.Domain/Models/Pais.cs
+++ instituto93.Domain/Models/Pais.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using instituto93.Domain.Interfaces;
 
 namespace instituto93.Domain.Models
 {
-    public class Pais
+    public class Pais : IPais
     {
         public int Id { get; set; }
-        public string Nombre { get; set; } = string.Empty;
-        public string Abreviatura { get; set; } = string.Empty;
+        public string Nombre { get; set; }
+        public string Abreviatura { get; set; }
     }
 }
