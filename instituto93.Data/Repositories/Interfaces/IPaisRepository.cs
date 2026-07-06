f287346d1c57346659e9817a9ed52369b754ba74
nicos92 <nicolassandoval140692@gmail.com>
2026-07-06 07:03:42 -0300

    Agrega interfaz IPaisRpository en la capa Data

---
+++ instituto93.Data/Repositories/Interfaces/IPaisRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using instituto93.Domain.Models;
+
+namespace instituto93.Data.Repositories
+{
+    public interface IPaisRepository
+    {
+        Task<IEnumerable<Pais>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<Pais?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<int> CreateAsync(Pais pais, CancellationToken cancellationToken = default);
+        Task<bool> UpdateAsync(Pais pais, CancellationToken cancellationToken = default);
+        Task<bool> DeleteAsync(int id, CancellationToken cancellationToken = default);
+    }
+}
